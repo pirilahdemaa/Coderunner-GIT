74e44e2d66b2de7607ac40ad6f298e24a03f0757
pirilahdemaa <90548746+pirilahdemaa@users.noreply.github.com>
2021-11-24 20:24:20 +0200

    Add files via upload

---
+++ AutotJaOliot.java
@@ -0,0 +1,41 @@
+
+public class AutotJaOliot {
+
+	public static void main(String[] args) {
+		// Coderunner ja Git-tehtävä
+
+	}//main
+
+}//class
+
+class Car
+{
+	public String brand;
+	public String model;
+	public int amountOfFuel;
+	
+	
+	public void brake()
+	{
+		System.out.println("Car is breaking");
+	}
+	
+	public void accelerate()
+	{
+		amountOfFuel = amountOfFuel -1;
+		
+		if (amountOfFuel > 0 )
+		{
+		System.out.println("Car is accelerating");
+		}
+	}
+	
+	public void printData()
+	{
+		System.out.println("Brand: "+brand);
+		System.out.println("Model: "+model);
+		System.out.println("Fuel: "+amountOfFuel);
+	}
+		
+		
+}//car
